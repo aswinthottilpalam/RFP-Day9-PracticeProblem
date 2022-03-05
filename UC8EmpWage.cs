720b18958d5fc5785b136d68459a939b5fcbd0e3
aswin26498 <85921826+aswin26498@users.noreply.github.com>
2022-03-05 22:44:46 +0530

    Day nine practice problems

---
+++ UC8EmpWage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC7_8_EmpWage
+{
+    internal class UC8EmpWage
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            //Varibles
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            //Computation
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs: " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs + empRatePerHour;
+            Console.WriteLine("Total Emp Wage for Company: " + company + " is: " + totalEmpWage);
+            return totalEmpWage;
+        }
+
+
+        static void Main(string[] args)
+        {
+            computeEmpWage("CompanyOne", 20, 2, 10);
+            computeEmpWage("CompanyTwo", 10, 4, 20);
+        }
+    }
+}
